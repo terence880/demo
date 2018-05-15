c4c343f16fa1483e26da479a626459bb2cb57f77
terence880 <terence380787@gmail.com>
2018-05-15 20:24:52 +0800

    final

---
+++ Desktop/pictionary-nodejs/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/lobby.html');
+});
+
+
+http.listen(8080);
